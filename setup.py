ddf365ac4527d5325b859f8710051518192a771f
Vitalii <vitaliyfilarskiy@gmail.com>
2025-07-16 19:26:11 +0200

    module setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+# from setuptools import setup
+
+# setup(
+#     name="ContactBot",
+#     version="0.1.0",
+#     packages=["NeoContactBot"],
+#     entry_points={
+#         "console_scripts": [
+#             "ContactBot = src.main:main",
+#         ],
+#     },
+# )
