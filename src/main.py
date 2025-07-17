8315354156a90460a4f9b5e683b51a78b6fd4b4d
Vitalii <vitaliyfilarskiy@gmail.com>
2025-07-17 10:20:24 +0200

    patient bot

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
-from handlers import add_contact, change_contact, show_phone, show_all, add_birthday, show_birthday, birthdays, add_email, add_address, delete_contact, find_contact, add_note, find_note, delete_note, edit_note, show_notes, show_help
+from handlers import (
+    add_contact, change_contact, show_phone, show_all, add_birthday, show_birthday,
+    birthdays, add_email, add_address, delete_contact, find_contact,
+    add_note, find_note, delete_note, edit_note, show_notes, show_help
+)
 from models import AddressBook, NoteBook
 from utils import parse_input
 from storage import save_data, load_data, save_notebook, load_notebook
@@ -9,8 +13,8 @@ init(autoreset=True)
 def main():
     book = load_data()
     notebook = load_notebook()
-    print(Fore.GREEN + "Welcome to your assistant bot!")
-    print(Fore.YELLOW + 'Please write "help" to see the commands\n')
+    print(Fore.GREEN + "Welcome to your assistant bot.")
+    print(Fore.YELLOW + 'Type "help" to view the list of available commands.\n')
 
     try:
         while True:
@@ -18,7 +22,7 @@ def main():
             command, args = parse_input(user_input)
 
             if command in ("exit", "close"):
-                print(Fore.GREEN + "Goodbye!")
+                print(Fore.GREEN + "Session ended. Goodbye!")
                 break
             elif command == "add":
                 print(Fore.GREEN + add_contact(args, book))
@@ -27,9 +31,9 @@ def main():
             elif command == "add-note":
                 print(Fore.GREEN + add_note(args, notebook))
             elif command == "all":
-                print(Fore.MAGENTA + show_all(book))
+                print(Fore.YELLOW + show_all(book))
             elif command == "birthdays":
-                print(Fore.MAGENTA + birthdays(book))
+                print(Fore.YELLOW + birthdays(book))
             elif command == "change":
                 print(Fore.GREEN + change_contact(args, book))
             elif command == "phone":
@@ -37,7 +41,7 @@ def main():
             elif command == "show-birthday":
                 print(Fore.YELLOW + show_birthday(args, book))
             elif command == "find-note":
-                print(Fore.MAGENTA + find_note(args, notebook))
+                print(Fore.YELLOW + find_note(args, notebook))
             elif command == "del-note":
                 print(Fore.RED + delete_note(args, notebook))
             elif command == "del-contact":
@@ -45,15 +49,15 @@ def main():
             elif command == "edit-note":
                 print(Fore.GREEN + edit_note(args, notebook))
             elif command == "show-notes":
-                print(Fore.MAGENTA + show_notes(notebook))
+                print(Fore.YELLOW + show_notes(notebook))
             elif command == "hello":
-                print(Fore.CYAN + "How can I help you?")
+                print(Fore.CYAN + "Hello! How can I assist you today?")
             elif command == "help":
                 print(Fore.YELLOW + show_help())
             else:
-                print(Fore.RED + "Unknown command. Try again.")
+                print(Fore.RED + "Sorry, I did not recognize that command. Please try again.")
     except KeyboardInterrupt:
-        print(Fore.RED + "\nInterrupted. Saving data and exiting.")
+        print(Fore.RED + "\nSession interrupted. Saving your data and exiting...")
     finally:
         save_data(book)
         save_notebook(notebook)
